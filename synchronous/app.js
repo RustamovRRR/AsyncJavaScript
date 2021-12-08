3e4ae3b41d75bb3b89e1fc39ac933561c49e5c50
RustamovRRR <rustamrrustamov@gmail.com>
2021-12-08 21:38:50 +0400

    understanding diffuculty of synchonous things

--- synchronous/app.js
+++ synchronous/app.js
@@ -1,16 +1,37 @@
 // JavaScript is simple threaded, synchronous language
 
-console.log(`I'm first`);
-console.log(`I'm second`);
-console.log(`I'm third`);
-boilingWater();
-console.log(`I'm first`);
-console.log(`I'm second`);
-console.log(`I'm third`);
+// console.log(`I'm first`);
+// console.log(`I'm second`);
+// console.log(`I'm third`);
+// boilingWater();
+// console.log(`I'm first`);
+// console.log(`I'm second`);
+// console.log(`I'm third`);
 
-function boilingWater() {
+// function boilingWater() {
+//   console.log("boiling water");
+//   for (let i = 0; i < 10000; i++) {
+//     console.log("still boiling");
+//   }
+//   console.log("done");
+// }
+
+// Make Soup
+// chop onion
+// chop carrot
+// boil water 10 minutes
+// add carrot boil for 5 minutes
+// add onion boil for 5 minutes
+
+boilWater(10000);
+console.log("chop carrot");
+boilWater(5000);
+console.log("chop onion");
+boilWater(5000);
+
+function boilWater(time) {
   console.log("boiling water");
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < time; i++) {
     console.log("still boiling");
   }
   console.log("done");
